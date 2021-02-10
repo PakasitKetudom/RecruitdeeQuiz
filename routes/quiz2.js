187015f1f4ce7e0b7d129ddb791b419a0781a1c3
PakasitKetudom <ke.pakasit@gmail.com>
2021-02-10 11:18:11 +0700

    second commit

--- routes/quiz2.js
+++ routes/quiz2.js
@@ -5,14 +5,10 @@ var request = require('request');
 
 let url = 'https://api.publicapis.org/categories';
 
-// router.get('/', function(req, res, next) {
-//   res.render('quiz2', { title: 'Portfolio' });
-// });
-
 router.get('/', function(req, res) {
     request(url, function(error, response, body) {
-      const info = JSON.parse(body);
-      res.render('quiz2', { info });
+      const categories = JSON.parse(body);
+      res.render('quiz2', { categories });
     });
   });
 
